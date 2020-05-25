14bac3c55c375a53365d5c88f20e40b6e44b1dd5
jose-lr <65802272+jose-lr@users.noreply.github.com>
2020-05-24 23:05:10 -0500

    Update adherentcard_create.tpl.php

--- htdocs/adherents/canvas/default/tpl/adherentcard_create.tpl.php
+++ htdocs/adherents/canvas/default/tpl/adherentcard_create.tpl.php
@@ -49,8 +49,11 @@ echo $this->control->tpl['ajax_selectcountry']; ?>
 <?php } ?>
 
 <table class="border allwidth">
+	<caption>New</caption>
 
 <tr>
+	<th scope="col">Name</th>
+    	<th scope="col">Age</th>
 	<td width="15%" class="fieldrequired"><?php echo $langs->trans("Lastname").' / '.$langs->trans("Label"); ?></td>
 	<td><input name="lastname" type="text" size="30" maxlength="80" value="<?php echo $this->control->tpl['name']; ?>"></td>
 	<td width="20%"><?php echo $langs->trans("Firstname"); ?></td>
